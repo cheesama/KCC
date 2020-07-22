818b761b94692a69d1a4a3af938fd3130349d48b
Ubuntu <ltech@rasalt-dev.p1nvgwc5saqe3ass43yrm55tjc.syx.internal.cloudapp.net>
2020-07-22 03:52:46 +0000

    add debug print for fixing bug

--- KCC/kcc_lightning_model.py
+++ KCC/kcc_lightning_model.py
@@ -165,10 +165,13 @@ class KcBERT_Classifier(pl.LightningModule):
         intent_acc = get_accuracy(intent_pred.argmax(1), intent_idx)[0]
         intent_f1 = f1_score(intent_pred.argmax(1), intent_idx)
 
+        print (entity_idx.size())
+        print (entity_pred.size())
+
         if entity_idx.sum().item() == 0 or torch.tensor(entity_pred).sum().item() == 0:
-            entity_acc = get_token_accuracy(entity_idx.cpu(), torch.tensor(entity_pred).cpu())[0]
+            entity_acc = get_token_accuracy(entity_idx.cpu(), entity_pred.cpu())[0]
         else:
-            entity_acc = get_token_accuracy(entity_idx.cpu(), torch.tensor(entity_pred).cpu(), ignore_index=self.dataset.pad_token_id)[0]
+            entity_acc = get_token_accuracy(entity_idx.cpu(), entity_pred.cpu(), ignore_index=self.dataset.pad_token_id)[0]
 
         intent_loss = self.intent_loss_fn(intent_pred, intent_idx.long(),)
         entity_loss = self.entity_loss_fn(entity_pred.transpose(1, 2), entity_idx.long(),)
